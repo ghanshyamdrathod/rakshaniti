6da691e6549d4800fcd1925280dd37b6d81b982b
ghanshyam Rathod <ghanshyam.d.rathod@gmail.com>
2015-06-26 17:46:09 +0530

    [ADD] Initial commit

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,24 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret 'a01208755337531a05c57ee065a5ccb42a293375f34249d5b6fb376ab1575195'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rakshaniti::Application.config.secret_token = '58848fb00d6da76a93619814a2da08e06477e4c3e68baa3672956419c15dc1159595dfbd1962ce848facfa11b41be373034a454aece67aa5b8ba4faab4bf8c83'
